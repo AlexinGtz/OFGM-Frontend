c94b2e30e680424862d5457eff052e1a609f1e24
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-06-26 20:57:02 -0500

    a

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 npm ci
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install
-runEnv=$(echo $1 | tr "/" "\n" | tail -2 | head -1)
+runEnv=$1#$(echo $1 | tr "/" "\n" | tail -2 | head -1)
 export AWS_ACCESS_KEY_ID=$2
 export AWS_SECRET_ACCESS_KEY=$3
 
