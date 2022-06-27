d62aa49ed9f6f790ccabf5cc95cd41be9e8ca216
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-06-26 20:56:14 -0500

    Updated script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,23 @@ export AWS_SECRET_ACCESS_KEY=$3
 
 npm run build
 
-aws s3 sync ./build s3://ofgm-backend-qa-ofgmwebpage-9l73f84jk8mg --region=us-east-1
-aws cloudfront create-invalidation --distribution-id E1RKAY3OTOGCHL --paths '/*' --region=us-east-1
+
+if [[ $runEnv = 'qa' ]]    # ← see 'man test' for available unary and binary operators.
+then
+    S3_BUCKET='ofgm-backend-qa-ofgmwebpage-9l73f84jk8mg'
+    CF_DIST='E1RKAY3OTOGCHL'
+elif [[ $runEnv = 'prod' ]]
+then
+    S3_BUCKET='testBucekt'
+    CF_DIST='test?Dist'
+else
+    echo 'environment in the tag not valid'
+    exit 1
+fi
+
+echo ${S3_BUCKET}
+echo ${CF_DIST}
+
+#aws s3 sync ./build s3://${S3_BUCKET} --region=us-east-1
+#aws cloudfront create-invalidation --distribution-id ${CF_DIST} --paths '/*' --region=us-east-1
 
