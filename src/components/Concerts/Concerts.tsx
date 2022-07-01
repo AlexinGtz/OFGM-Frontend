27f6538cb7ee25e16df3ef215d0f0981911d7796
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-07-01 17:05:25 -0500

    Added initial files

---
+++ src/components/Concerts/Concerts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Concerts.css'
+
+const Concerts = () => {
+  return (
+    <div>Concerts</div>
+  )
+}
+
+export default Concerts
--- src/screens/MainLayout.tsx
+++ src/screens/MainLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Concerts from "../components/Concerts/Concerts";
 import NavBar from "../components/NavBar/NavBar";
 import { OnConstruction } from "../components/OnConstruction/OnConstruction";
 
@@ -8,6 +9,7 @@ export const MainLayout = () => {
     <React.Fragment>
       <NavBar />
       <Routes>
+        <Route path="/concerts" element={<Concerts />} />
         <Route path="*" element={<OnConstruction />} />
       </Routes>
       {/* <Footer /> */}
