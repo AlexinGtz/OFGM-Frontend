c79a143b664c2ea239d39c8213a36cd1e6cd028c
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-07-13 12:34:09 -0500

    Changed API url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,9 +4,7 @@ export class API {
     client: AxiosInstance;
 
     constructor() {
-        const baseURL = process.env.NODE_ENV === 'development' ? 
-            'https://sgct180v33.execute-api.us-east-1.amazonaws.com/qa' 
-            : '';
+        const baseURL = 'https://sgct180v33.execute-api.us-east-1.amazonaws.com/qa'
         this.client = axios.create({
             baseURL,
             timeout: 10000,
